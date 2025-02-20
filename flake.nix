c91aa550739b6e412bf8879bfd28c1ad033d032a
Artemis Tosini <me@artem.ist>
2025-02-20 22:52:19 +0000

    Setup for nginx, easier builds

--- flake.nix
+++ flake.nix
@@ -1,18 +1,28 @@
 {
   inputs = {
-    nixbsd.url = "github:nixos-bsd/nixbsd/tmp";
+    nixbsd.url = "github:nixos-bsd/nixbsd/openbsd-phase6";
   };
   outputs =
     { self, nixbsd }:
     {
-      packages.x86_64-openbsd.theSystem = self.nixosConfigurations.theSystem;
-      nixosConfigurations.theSystem = nixbsd.nixosConfigurations.openbsd-base.extendModules {
+      packages.x86_64-linux.demo =
+        (self.nixosConfigurations.demo.extendModules {
+          modules = [
+            {
+              # fakeNative lets the OpenBSD system think it built the configuration itself.
+              # This doesn't work when we're doing the real cross build, so disable it.
+              nixpkgs.fakeNative = false;
+            }
+          ];
+        }).config.system.build.toplevel;
+      nixosConfigurations.demo = nixbsd.nixosConfigurations.openbsd-base.extendModules {
         modules = [
           (
             { config, lib, ... }:
             {
               nixpkgs.buildPlatform = "x86_64-linux";
-              nixpkgs.fakeNative = true;
+
+              nixpkgs.fakeNative = lib.mkDefault true;
 
               fileSystems."/" = {
                 device = "/dev/sd0a";
@@ -22,6 +32,15 @@
                 device = "/dev/sd0i";
                 fsType = "msdos";
               };
+
+              services.nginx = {
+                enable = true;
+                virtualHosts.localhost = {
+                  default = true;
+                  root = ./.;
+                };
+              };
+
             }
           )
         ];
