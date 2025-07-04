6005dbd4680e84cede60305d8c729a7e30232d65
Audrey Dutcher <audrey@rhelmot.io>
2025-07-04 14:12:58 -0700

    no more fakenative, please

--- flake.nix
+++ flake.nix
@@ -1,26 +1,22 @@
 {
   inputs = {
-    nixbsd.url = "github:obsidiansystems/nixbsd/9c0584fff8ae61946a46ab182148cf652559d364";
+    nixbsd.url = "github:obsidiansystems/nixbsd/openbsd-phase6";
   };
   outputs =
-    { self, nixbsd }:
-    let realCross = 
-      self.nixosConfigurations.demo.extendModules {
-        modules = [
-          {
-            # fakeNative lets the OpenBSD system think it built the configuration itself.
-            # This doesn't work when we're doing the real cross build, so disable it.
-            nixpkgs.fakeNative = false;
-          }
-        ];
-      };
-      installer = nixbsd.packages.x86_64-linux.openbsd-base;
-    in
-    {
+    { self, nixbsd }: {
       packages.x86_64-linux = {
-        demo-toplevel = realCross.config.system.build.toplevel;
-        installer-vm = installer.config.system.build.vm;
-        installer-image = installer.config.system.build.systemImage;
+        demo-toplevel = self.nixosConfigurations.demo.config.system.build.toplevel;
+        installer-vm = self.nixosConfigurations.installer.config.system.build.vm;
+        installer-image = self.nixosConfigurations.installer.config.system.build.systemImage;
+      };
+      nixosConfigurations.installer = nixbsd.nixosConfigurations.openbsd-base.extendModules {
+        modules = [
+          ({pkgs, ...}: {
+            nixpkgs.buildPlatform = "x86_64-linux";
+            nix.settings.trusted-users = [ "demo" ];
+            environment.systemPackages = [ pkgs.openssh ];
+          })
+        ];
       };
       nixosConfigurations.demo = nixbsd.nixosConfigurations.openbsd-base.extendModules {
         modules = [
@@ -28,8 +24,6 @@
             { config, lib, ... }:
             {
               nixpkgs.buildPlatform = "x86_64-linux";
-
-              nixpkgs.fakeNative = lib.mkDefault true;
 
               fileSystems."/" = {
                 device = "/dev/sd0a";
